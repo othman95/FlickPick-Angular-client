50e53b7ff665f3693550ea81bfd79c185239d3cd
codestun <doerthy.k@gmail.com>
2024-01-12 12:21:31 +0100

    Implement user login functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { MatDialog } from '@angular/material/dialog';
-
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
@@ -17,6 +17,11 @@ openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, {
     // Assigning the dialog a width
     width: '280px'
+    });
+  }
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+      width: '280px',
     });
   }
 }
---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router'; // Import Router
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss']
+})
+export class UserLoginFormComponent implements OnInit {
+
+  @Input() userData = { Name: '', Password: '' };
+
+  constructor(
+      public fetchApiData: FetchApiDataService,
+      public dialogRef: MatDialogRef<UserLoginFormComponent>,
+      public snackBar: MatSnackBar,
+      public router: Router) { }
+
+  ngOnInit(): void {}
+
+  loginUser(): void {
+    console.log('Login');
+    const credentials = {
+      Name: this.userData.Name,
+      Password: this.userData.Password
+    };
+    // Pass the credentials object to userLogin
+    this.fetchApiData.userLogin(credentials).subscribe({
+      next: (result) => {
+        localStorage.setItem('user', JSON.stringify(result.user));
+        localStorage.setItem('token', result.token);
+
+        // Display a success message
+      this.snackBar.open('Login successful', 'OK', {
+        duration: 2000,
+      });
+
+        this.dialogRef.close(); // Close the modal on success
+        this.router.navigate(['movies']);
+      },
+      error: (error) => {
+        // Handle error
+        this.snackBar.open(error, 'OK', {
+          duration: 2000,
+        });
+      }
+    });
+  }
+}
